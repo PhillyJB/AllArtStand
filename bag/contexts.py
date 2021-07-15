ee16e65ff5f01628ba6e439d8e114b3e7aefb7f3
PhillyJB <philipbuuza@gmail.com>
2021-07-15 12:35:05 +0000

    updated shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from gallery.models import Art_Pieces
+
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    art_count = 0
+    bag = request.session.get('bag', {})
+
+    for art_id, quantity in bag.items():
+        art = get_object_or_404(Art_Pieces, pk=art_id)
+        total += quantity * art.price
+        art_count += quantity
+        bag_items.append({
+            'art_id': art_id,
+            'quantity': quantity,
+            'art': art,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
@@ -19,7 +33,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'art_count': art_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
